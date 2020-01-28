81967cda953c31fcd0f2af8fc40733d19b287cfb
4myc <missch3n@gmail.com>
2020-01-28 10:59:03 -0800

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react'
+// import React, { Component } from 'react';
 import learnSymbol from './data.js'
 import Cell from './Cell.js'
 import ColorSelector from './ColorSelector.js'
 
-export default class Matrix extends Component {
+// Matrix (parent component) keeps track of the current selected color
+// Work from the 'top' layer down
+
+// DELIVERABLES:
+// A way for Matrix to keep track of the selected color (think state!)
+// A method that takes in a single argument of a hexadecimal color string (i.e. '#fff') and sets the selected color to that
+
+// figure out how to use the component's state
+// how to use the method that will update that state, in the ColorSelector and Cell components
+
+class Matrix extends React.Component {
 
   constructor() {
     super()
+    // STEP 1: Set up state with a default color ("#FFF") to keep track of selected color
+    this.state = {
+      selectedColor: "#FFF"
+    }
   }
 
+  // STEP 2: Add a method to update the state value:
+  // this method updates selectedColor with whatever is passed into it as an argument
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+
+  // returns a collection of Cell components
+  // STEP 4: Pass currently selected color in to every Cell returned by genRow() as a prop called selectedColor 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
   )
 
   genMatrix = () => (
@@ -20,8 +45,11 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+      {/* ColorSelector is the interface where users will be able to select a color, so it will need access to setSelectedColor */}
+      {/* STEP 3: Pass Data and Callbacks to Children by passing the needed function down as a prop: */}
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
         <div id="matrix">
+        {/* Remember that when using arrow functions, you can use parentheses instead of curly braces to implicitly return a value */}
           {this.genMatrix()}
         </div>
       </div>
@@ -32,3 +60,5 @@ export default class Matrix extends Component {
 Matrix.defaultProps = {
   values: learnSymbol
 }
+
+export default Matrix
