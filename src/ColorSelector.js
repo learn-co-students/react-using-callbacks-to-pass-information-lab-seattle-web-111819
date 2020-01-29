0c71f8179ba876939434f319d17db6306e7693b3
4myc <missch3n@gmail.com>
2020-01-28 22:39:08 -0800

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -12,10 +12,10 @@ class ColorSelector extends Component {
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
 
-      // STEP 6: need to write the function inside the map to access the color values needed:
+      // STEP 5: need to write the function inside the map to access the color values needed:
       let callback = () => this.props.setSelectedColor(str)
 
-      // STEP 5: when any one of these divs is clicked the hexadecimal color value of that div becomes the selected color in Matrix.
+      // STEP 6: when any one of these divs is clicked the hexadecimal color value of that div becomes the selected color in Matrix.
       return <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
       // Each div element is rendered from an array of hexadecimal color strings
       // backgroundColor set to the value of the current string in the mapping process
